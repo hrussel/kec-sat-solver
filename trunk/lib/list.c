52e772f85bcd00f2213a35159e4e77988c3f0d9d
kelwinfc <kelwinfc@47ac6b63-da99-f6ec-0d68-1e7f8b9bf807>
2011-01-31 04:02:56 +0000

    Se hizo: - Un tipo lista generico. - Un esbozo del solver. - Diseño e inicialización de las estructuras del SAT solver. - Parser de DIMACS, con su transformación a las estructuras del SAT solver.
        Que falta:
        1- Asignar variable.
        2- Eliminar asignación.
        3- Unit Propagation.
        3- Pure literal Elimination.

---
+++ trunk/lib/list.c
@@ -0,0 +1,148 @@
+#include "list.h"
+
+void initialize_list(list* l){
+    l->first = l->last = NULL;
+    l->size = 0;
+}
+
+void push(list* l, void* item){
+    
+    node* new_node;
+    
+    new_node = (node*)malloc(sizeof(node));
+    
+    if (new_node == NULL){
+        printf("Error: malloc\n");
+        exit(1);
+    }
+    
+    new_node->item = item;
+    new_node->next = l->first;
+    new_node->prev = NULL;
+    
+    if (!empty(l)){
+        l->first->prev = new_node;
+    } else {
+        l->last = new_node;
+    }
+    
+    l->first = new_node;
+    l->size++;
+}
+
+void* top(list* l){
+    if (!empty(l)){
+        return l->first->item;
+    } else {
+        return 0;
+    }
+}
+
+void pop(list* l){
+    
+    if (!empty(l)){
+        
+        node* next = l->first->next;
+        
+        free(l->first);
+        
+        l->first = next;
+        
+        if (l->first != NULL){
+            l->first->prev = NULL;
+        }
+        
+        l->size--;
+        
+        if (empty(l)){
+            l->last = NULL;
+        }
+    }
+}
+
+int empty(list* l){
+    return l->size == 0;
+}
+
+void multi_pop(list* l, int n){
+    while(n-- && !empty(l)){
+        pop(l);
+    }
+}
+
+void queue(list* l, void* item){
+    node* new_node;
+    
+    new_node = (node*)malloc(sizeof(node));
+    new_node->item = item;
+    new_node->prev = l->last;
+    new_node->next = NULL;
+    
+    if (l->last){
+        l->last->next = new_node;
+    }
+    
+    l->last = new_node;
+    
+    if (empty(l)){
+        l->first = new_node;
+    }
+    
+    l->size++;
+}
+
+void dequeue(list* l, void* item){
+    if (l->last){
+        
+        node* next = l->first->next;
+        
+        free(l->first);
+        
+        l->first = next;
+        
+        if (l->first){
+            l->first->prev = 0;
+        }
+        
+        l->size--;
+        
+        if (l->size == 0){
+            l->last = 0;
+        }
+    }
+}
+
+void print(list* l){
+    node* next = l->first;
+    printf("Size %d\n",l->size);
+    while(next){
+        printf("%d\n", *((int*)next->item));
+        next = next->next;
+    }
+}
+
+/*
+int main(){
+    list l;
+    new_list(&l);
+    
+    int num = 1;
+    int num2 = 2;
+    int num3 = 3;
+    push(&l, &num);
+    push(&l, &num2);
+    push(&l, &num3);
+    print(&l);
+    pop(&l);
+    print(&l);
+    push(&l, &num3);
+    print(&l);
+    multi_pop(&l,3);
+    push(&l, &num);
+    push(&l, &num2);
+    push(&l, &num3);
+    print(&l);
+    multi_pop(&l,7);
+    return 0;
+}
+*/
---
+++ trunk/lib/sat.h
@@ -0,0 +1,82 @@
+#ifndef SAT_
+
+    #define SAT_
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "list.h"
+
+#define TRUE 1
+#define FALSE 0
+#define UNKNOWN -1
+
+#define CONFLICT 1
+
+#define BUFFERSIZE 10123
+
+typedef int variable;
+
+typedef list stack;
+
+typedef struct clause{
+    int size;
+    variable* head_watcher;
+    variable* tail_watcher;
+    variable* literals;
+    int satisfied;
+} clause;
+
+typedef struct SAT_status{
+    
+    int num_vars, num_clauses;
+    clause *formula;
+    
+    list *pos_occurrence_list;
+    list *neg_occurrence_list;
+    
+    stack backtracking_status;
+    
+    char *model;
+    int status;
+    
+} SAT_status;
+
+typedef struct decision_level_data{
+    
+    variable assigned_literal;
+    int missing_branch;             // true iff we need to test the opposite
+                                    // polarity of the decision variable.
+    
+    list propagated_var;
+    list satisfied_clauses;
+    
+} decision_level_data;
+
+void set_initial_sat_status(char filename[]);
+
+void set_clause(clause* cl, int clause_length, int literals[]);
+
+void print_formula();
+
+/* Asigna la variable con su polaridad correspondiente en la formula.
+
+   Status:
+   Si la formula es satisfecha retornar TRUE
+   Si hay contradiccion retornar FALSE
+   En caso contrario retornar UNKNOWN
+   
+   Asignar nuevos watched literals en las clausulas en las que ella sea un
+   watched literal.
+ */
+int assign(variable literal);
+
+void unassign(variable literal);
+
+int decide_next_branch();
+
+int solver();
+
+SAT_status sat_st;
+
+#endif
