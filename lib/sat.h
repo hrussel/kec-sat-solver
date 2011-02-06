4c30fb9d529574252cc022ce4d54428cc462bd7e
kelwinfc <kelwinfc@47ac6b63-da99-f6ec-0d68-1e7f8b9bf807>
2011-02-06 14:56:02 +0000

    Comentarios a las estructuras de sat.h

--- lib/sat.h
+++ lib/sat.h
@@ -21,6 +21,17 @@ typedef int variable;
 
 typedef list stack;
 
+/**
+*
+*  This struct models a clause in the formula.
+*
+*  @param size The number of literals in the clause.
+*  @param head_watcher Pointer to one of the clause literals, that is being watched.
+*  @param tail_watcher Pointer to one of the clause literals, that is being watched.
+*  @param literals An array of the literals occurring in the clause.
+*  @param satisfied True iff the clause is already satisfied.
+*
+*/
 typedef struct clause{
     int size;
     variable* head_watcher;
@@ -29,9 +40,29 @@ typedef struct clause{
     int satisfied;
 } clause;
 
+/**
+*  This struct models the current state of analysis of the problem. There is a
+*  single global variable named sat_st of this type, on which the necessary
+*  changes for this analysis will be perfomed.
+*
+*  @param num_vars The number of distinct literals in the whole formula.
+*  @param num_clauses The number of clauses in the formula.
+*  @param pos_occurrence_list An array A such that A[i] is the list of clauses 
+*         where the variable x_i occurs positively.
+*  @param neg_occurrence_list An array A such that A[i] is the list of clauses 
+*         where the variable x_i occurs negatively.
+*  @param backtracking_status Is a stack that holds the information 
+*         corresponding to a decision level. Its elements are of type 
+*         decision_level_data.
+*  @param model An array A such that A[i] holds the current assignment of 
+*         variable x_i. This assignment can be only of the following:
+*         TRUE, FALSE, UNKNOWN.
+*
+*/
 typedef struct SAT_status{
     
-    int num_vars, num_clauses;
+    int num_vars;
+    int num_clauses;
     clause *formula;
     
     list *pos_occurrence_list;
@@ -40,10 +71,23 @@ typedef struct SAT_status{
     stack backtracking_status;
     char *model;                     // The current assignment of boolean values
                                      // to literals, that is under study.
-    int status;
     
 } SAT_status;
 
+/**
+*  This struct models the applied changes in a certain decision level.
+*
+*  @param assigned_literal The first literal selected for assignment in a 
+*         decision level. This assignment may span other assigned_literals with
+*         the aid of Boolean Constrain Propagation (unit_propagation).
+*  @param missing_branch true iff we need to test the opposite
+*         polarity of the decision variable..
+*  @param propagated_var A list of variables whose assignment was inferred from
+*         the assignment of @e assigned_literal.
+*         where the variable x_i occurs positively.
+*  @param satisfied_clauses A list of the clauses that in the current
+*         decision level have already been satisfied.
+*/
 typedef struct decision_level_data{
     
     variable assigned_literal;
@@ -73,12 +117,14 @@ void print_formula();
    Asignar nuevos watched literals en las clausulas en las que ella sea un
    watched literal.
  */
-int assign(variable literal, char value, list* clauses_made_true);
+int assign(variable literal);
 
 void set_newly_satisfied_clauses( list* clauses_made_true );
 
-variable* unit_propagation( list* clauses_not_made_true,
-                            variable literal );
+int set_newly_unsatisfied_clauses( list* clauses_not_made_true,
+                                    variable literal );
+                                    
+int unit_propagation( stack* clauses_not_made_true);
 
 void unassign(variable literal);
 
