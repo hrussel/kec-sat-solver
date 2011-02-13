29ec380bd0d123b00f97acd229f6fe02202f6e89
kelwinfc <kelwinfc@47ac6b63-da99-f6ec-0d68-1e7f8b9bf807>
2011-02-13 22:22:16 +0000

    Agregada version inicial de traductor sudoku <==> sat

--- lib/pure_literals.h
+++ lib/pure_literals.h
@@ -1,5 +1,6 @@
-//#include "sat.h"
-variable* find_pure_literals( int* pure_literals_size );
+#include "sat.h"
+
+variable* find_pure_literals( int *pure_literals_size );
 
 void swap_literals( clause* cl, int old_index, int new_index );
 
