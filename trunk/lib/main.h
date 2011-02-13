7ad07688e0d53c959dd67d379d2fed2de39fe0e8
carlos.a.colmenares.r <carlos.a.colmenares.r@47ac6b63-da99-f6ec-0d68-1e7f8b9bf807>
2011-02-13 14:34:50 +0000

    Fixed the project's structure and added a pimped makefile

---
+++ trunk/lib/main.h
@@ -0,0 +1,6 @@
+#ifndef _KEC_SAT_MAIN_
+#define _KEC_SAT_MAIN_
+
+#include"sat.h"
+
+#endif
---
+++ trunk/lib/sat_io.h
@@ -0,0 +1,10 @@
+#ifndef _KEC_SAT_SAT_IO_
+#define _KEC_SAT_SAT_IO_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include"sat.h"
+
+
+
+#endif
