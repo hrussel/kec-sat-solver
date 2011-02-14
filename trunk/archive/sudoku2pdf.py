b0ddc39b6b18fd69c1a489c8d46285a0e6f198fe
carlos.a.colmenares.r <carlos.a.colmenares.r@47ac6b63-da99-f6ec-0d68-1e7f8b9bf807>
2011-02-14 17:57:52 +0000

    now the sudokus are better printed =D I added an example input file

--- trunk/archive/sudoku2pdf.py
+++ trunk/archive/sudoku2pdf.py
@@ -2,7 +2,7 @@ import sys
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 
-def draw_sudoku(size, top, left, nums, label, pdf):
+def draw_sudoku(size, top, left, sudoku, label, pdf):
     right = size + left
     bottom = top-size
 
@@ -12,7 +12,7 @@ def draw_sudoku(size, top, left, nums, label, pdf):
     box_height = size / 9 
 
     #Draw the label
-    fontSize = 18
+    fontSize = 12
     pdf.setFont('Times-Bold',fontSize)
     pdf.drawString(left+box_height*0.5, top+box_height*0.5, label)
 
@@ -28,35 +28,79 @@ def draw_sudoku(size, top, left, nums, label, pdf):
     #Draw numbers
     fontSize = 24
 
-    nums = [str((x%9)+1) for x in range(3**4) ]
-
     for row in range(0, 9):
         for col in range(0, 9):
-            num = nums[row * 9 + col]
+            num = sudoku[row * 9 + col]
             if num != '0':
                 pdf.drawString(left + col * box_height + box_height * 0.38, top - row * box_height - box_height * 0.65, num)
 
 
 if __name__ == "__main__":
     #Parse the arguments
-    if len(sys.argv) != 2:
-        print "You must specify an output filename (and only one)"
+    if len(sys.argv) != 3:
+        print "usage:",sys.argv[0],"input_file output_file"
         sys.exit(1)
         
 
-    filename = sys.argv[1]
-    pdf = canvas.Canvas(filename)
+    input_file = open(sys.argv[1],'r')
+    output_file = sys.argv[2]
+    pdf = canvas.Canvas(output_file)
 
     width, height = letter
+    #Size of a sudoku
+    size = width*6.0/16.0
 
-    size = height*6.0/16.0
-    left = (width - size)/2.0
-    top1 = height*15.0/16.0
-    top2 = height*7.0/16.0
+    #The sudokus can be in the upper or lower part of the page
+    upper_y = height*0.75 + size*0.5
+    lower_y = height*0.25 + size*0.5
+    #The sudokus can be centered, to the left or right
+    center_x = width*0.5 - size*0.5
+    left_x = width*0.25 - size*0.5
+    right_x = width*0.75 - size*0.5
 
-    nums = [str((x%9)+1) for x in range(3**4) ]
+    #Read the number of test cases
+    cases = int(input_file.readline())
 
-    draw_sudoku(size, top1, left, nums, "Puzzle One", pdf)
-    draw_sudoku(size, top2, left, nums, "Puzzle Two", pdf)
+    for case_num in range(1,cases+1):
+        if case_num != 1:
+            pdf.showPage()
+
+        #Print case num
+        pdf.setFont('Times-Bold',15)
+        pdf.drawString(width*0.03,height*0.97,"Case #"+str(case_num))
+
+        #Cardinality of the sudoku
+        sudoku_card = int(input_file.readline())
+
+        #Sudoku to solve (semi-empty instance)
+        sudoku = (input_file.readline()).split()
+
+        #The base sudoku is centred and in the top
+        left = center_x
+        top = upper_y
+
+        draw_sudoku(size, top, left, sudoku, "Base Puzzle", pdf)
+
+        #Now read the number of other solutions
+        solutions = int(input_file.readline())
+
+        for sol_num in range(solutions):
+            #Calculate if it's a new page!
+            if sol_num%4 == 2:
+                pdf.showPage()
+
+            #Read the name of the solver and the time it performed
+            solver_name = input_file.readline().strip()
+            sol_time_ms = input_file.readline().strip() + "ms"
+
+            #Read the solution
+            sudoku = input_file.readline().split()
+
+            #Set the corners            
+            left = [left_x,right_x][sol_num%2]
+            top = [lower_y,lower_y,upper_y,upper_y][sol_num%4]
+
+            label = solver_name + "'s solution ("+sol_time_ms+")"
+            draw_sudoku(size, top, left, sudoku, label, pdf)
 
     pdf.save()
