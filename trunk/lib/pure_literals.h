26bb174f701948d22f1d5ec42e629fbfad056972
carlos.a.colmenares.r <carlos.a.colmenares.r@47ac6b63-da99-f6ec-0d68-1e7f8b9bf807>
2011-02-15 02:46:29 +0000

    added some documentation to the pure_literals

--- trunk/lib/pure_literals.h
+++ trunk/lib/pure_literals.h
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * KECOSATS (Kelwin-Eleazar-Carlos Optimized SAT Solver)
+ *
+ * Definition for the procedures that detect pure literals in clauses and
+ * assign them with values so as to simplify the SAT formula.
+ *
+ * @author Colmenares, Carlos (06-39380@usb.ve)
+ * @author Fernandez, Kelwin (07-40888@usb.ve)
+ * @author Leal, Eleazar (06-39780@usb.ve)
+ *
+ ******************************************************************************/
+#ifndef _KECOSATS_PURE_LITERALS_
+#define _KECOSATS_PURE_LITERALS_
+
+#include <stdio.h>
 #include "sat.h"
 
 variable* find_pure_literals( int *pure_literals_size );
@@ -8,3 +23,5 @@ void swap_clauses( clause* formula, int old_index, int new_index );
 
 int is_pure_literal( variable literal, variable* pure_literals,
                      int num_pure_literals );
+
+#endif
